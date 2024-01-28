bf377aabc40e9af12acda278ffc5d9756c590c7f
shreyesk <sshreyes.kaliyur@gmail.com>
2024-01-28 01:51:15 -0600

    day in database

--- boiler_server/database.py
+++ boiler_server/database.py
@@ -47,6 +47,9 @@ def create_user(name, sub, money=10000):
     if not document:
         profiles.insert_one(query_to_insert)
 
+def buy_stock(name, ticker_symbol):
+    pass
+
 def create_stock(ticker_symbol):
     stocks = client.stocks
     prices = stocks.prices
@@ -59,7 +62,22 @@ def create_stock(ticker_symbol):
         stock_prices = stock_data.get_stock_price(ticker_symbol)
         prices.insert_one({"ticker_symbol": ticker_symbol, "prices": stock_prices})
 
-def read_stock(ticker_symbol, day):
+def read_stock_price(ticker_symbol):
+    stocks = client.stocks
+    prices = stocks.prices
+
+    query = {"ticker_symbol": ticker_symbol}
+
+    document = prices.find_one(query)
+
+    day = get_day()
+    if document:
+        return document["prices"][day]
+    else:
+        create_stock(ticker_symbol)
+        return read_stock_price(ticker_symbol)
+
+def read_stock_history(ticker_symbol, days):
     stocks = client.stocks
     prices = stocks.prices
 
@@ -68,7 +86,92 @@ def read_stock(ticker_symbol, day):
     document = prices.find_one(query)
 
     if document:
-        return document["prices"][day]
+        return document["prices"][:days]
     else:
         create_stock(ticker_symbol)
-        return read_stock(ticker_symbol, day)
+        return read_stock_history(ticker_symbol, days)
+
+def create_group(group_name, user_name):
+    users = client.users
+    groups = users.groups
+
+    query = {"name": group_name}
+
+    document = groups.find_one(query)
+
+    if not document:
+        groups.insert_one({"name": group_name, "owner": user_name, "users": [user_name]})
+
+def read_group(name):
+    users = client.users
+    groups = users.groups
+    
+    query = {"name": name}
+
+    document = groups.find_one(query)
+
+    return document
+
+def join_group(group_name, user_name):
+    users = client.users
+    groups = users.groups
+
+    query = {"name": group_name}
+
+    document = groups.find_one(query)
+    current_users = document['users']
+
+    if document:
+        if user_name not in current_users:
+            update = {"$set": {"users": current_users + [user_name]}}
+            groups.update_one(query, update)
+
+
+def leave_group(group_name, user_name):
+    users = client.users
+    groups = users.groups
+
+    query = {"name": group_name}
+
+    document = groups.find_one(query)
+    current_users = document['users']
+
+    if document:
+        if user_name in current_users:
+            new_users = list(set(current_users) - set([user_name]))
+            update = {"$set": {"users": new_users}}
+            groups.update_one(query, update)
+
+def delete_group(group_name, user_name):
+    users = client.users
+    groups = users.groups
+
+    query = {"name": group_name}
+
+    document = groups.find_one(query)
+
+    if document:
+        if document['owner'] == user_name:
+            groups.delete_one(query)
+
+def read_day():
+    stocks = client.stocks
+    day = stocks.day
+    document = day.find_one()
+    return document['day']
+
+def increment_day():
+    stocks = client.stocks
+    day = stocks.day
+    document = day.find_one()
+    query = {"day": document['day']}
+    update = {"$set": {"day": document['day'] + 1}}
+    day.update_one(query, update)
+
+def reset_day():
+    stocks = client.stocks
+    day = stocks.day
+    document = day.find_one()
+    query = {"day": document['day']}
+    update = {"$set": {"day": 0}}
+    day.update_one(query, update)
