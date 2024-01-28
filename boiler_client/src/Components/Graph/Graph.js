696135e0e4ce83e7763fd6f85e47d00684cd8188
Jasonsze <jasonsze@tamu.edu>
2024-01-28 02:51:26 -0600

    changed some css

--- boiler_client/src/Components/Graph/Graph.js
+++ boiler_client/src/Components/Graph/Graph.js
@@ -3,19 +3,14 @@ import Stack from '@mui/material/Stack';
 import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
 import * as React from 'react';
 
-export default function BasicSparkLineCustomization({data, hoverInfo}) {
-  
-
+export default function BasicSparkLineCustomization({ data, hoverInfo }) {
   return (
-    <Stack direction="column" sx={{ width: '100%' }}>
-      <Stack direction="row">
-        
-      </Stack>
-      <Stack direction="row" sx={{ width: '100%' }}>
+    <Stack direction="column" sx={{ width: '100%', height: '100%' }}>
+      <Stack direction="row"></Stack>
+      <Stack direction="row" sx={{ width: '100%', height:'100%' }}>
         <Box sx={{ flexGrow: 1 }}>
           <SparkLineChart
             data={data}
-            height={100}
             showHighlight={hoverInfo}
             showTooltip={hoverInfo}
           />
--- boiler_client/src/Pages/Home/Home.js
+++ boiler_client/src/Pages/Home/Home.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Graph from '../../Components/Graph/Graph';
-import "./Home.css";
 import { fetchPost } from '../../util/fetchHelp';
+import "./Home.css";
 
 const Home = () => {
     // const [stockData, setStockData] = useState({"APPL": [3, [1, 2, 3, 4, 5, 20]], "TSLA": [4, [6, 5, 4, 3, 12, 2]]})
     const [stockData, setStockData] = useState();
-
+    const [wealthData, setWealthData] = useState();
     const postStockData = {"ticker_symbol": 'AAPL', 'days': 30};
-
+    //const postWealthData = {}
+    
+    //NEED A POST REQUEST TO GET USER STOCKS!
     useEffect(() => {
         fetchPost("/read_stock_history", postStockData).then(data => {
-            console.log(data);
             setStockData({'AAPL': [1, data['history']]});
         })
+        
     }, []); // Empty dependency array ensures this runs once
-
+    // useEffect(() => {
+    //     fetchPost("/read_stock_history", postWealthData).then(data => {
+    //         setStockData({'AAPL': [1, data['history']]});
+    //     })
+        
+    // }, []);
     return (
         <div>
             <div className='Main-Container'>
@@ -26,9 +33,13 @@ const Home = () => {
                                 <p>Stock: {symbol} ({price[0]})</p>
                                 <p>Price: {price[1][price[1].length - 1]}</p>
                                 {price[1][price[1].length - 1] - price[1][price[1].length - 2] >= 0 ? (
-                                    <p style={{color:"green"}}>+{(((100 *(price[1][price[1].length - 1] - price[1][price[1].length - 2])) / price[1][price[1].length - 2]) - 100).toFixed(2)}% </p>
+                                    <p style={{color:"green"}}>
+                                        +{(((price[1][price[1].length - 1] - price[1][price[1].length - 2]) / price[1][price[1].length - 2]) * 100).toFixed(2)}%
+                                    </p>
                                 ) : (
-                                    <p style={{color:"red"}}>{(((100 *(price[1][price[1].length - 1] - price[1][price[1].length - 2])) / price[1][price[1].length - 2]) - 100).toFixed(2)}%</p>
+                                    <p style={{color:"red"}}>
+                                        {(((price[1][price[1].length - 1] - price[1][price[1].length - 2]) / price[1][price[1].length - 2]) * 100).toFixed(2)}%
+                                    </p>
                                 )}
                             </div>
                             <div className='Stock-Graph'>
@@ -39,7 +50,10 @@ const Home = () => {
                     ))}
                 </div>
                 <div className='Graph-Container'>
-                    <Graph data={[1, 2, 6, 5, 9, 4, 5]} hoverInfo={true}/>
+                    <div className='Graph-Holder'>
+                        <Graph data={[175.46, 172.88, 173.0, 173.44, 171.1, 166.89, 168.22, 170.29, 170.77, 173.97, 177.57, 176.65, 179.23, 181.82, 182.89, 182.41, 186.4, 184.8, 187.44, 188.01, 189.71, 189.69, 191.45, 190.64, 191.31, 189.97, 189.79, 190.4, 189.37, 189.95]} hoverInfo={true}/>
+                    </div>
+                   
                 </div>
             </div>
         </div>
